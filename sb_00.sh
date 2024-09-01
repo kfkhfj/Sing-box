74d7d6d9aaf9a4813db7af698ab99568d298362b
kfkhfj <kfkhfj@gmail.com>
2024-09-01 09:06:56 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -13,7 +13,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 export LC_ALL=C
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'4456ff58-9e5c-4991-9f80-53df9a7ab387'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
